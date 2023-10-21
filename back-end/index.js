126060354194dc7f9ca8353d0c63f93014a9fbf0
Chris Mastris <cmastris@hotmail.com>
2023-10-21 15:29:56 +0100

    Configure logging using morgan

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const logging = require("morgan");
 
 const api = express();
 const port = process.env.PORT;
+
+api.use(logging(process.env.LOGGING));
 
 api.get("/", (req, res) => {
   res.send("Hello World!");
