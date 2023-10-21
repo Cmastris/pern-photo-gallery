c9966169a6dc3a64a4da3ec98f89ba08a1a44f46
Chris Mastris <cmastris@hotmail.com>
2023-10-21 17:25:12 +0100

    Create `getPhotos()` query function

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -7,4 +7,21 @@ const query = (text, params) => {
   return pool.query(text, params);
 };
 
-module.exports = query;
+
+const getPhotos = async (collection_id=undefined) => {
+  const select = "SELECT id, title, slug, summary_text, detail_text, location, date_taken, filename FROM photos";
+  let res;
+  if (collection_id) {
+    const join = "JOIN photo_collections ON photos.id=photo_collections.photo_id";
+    const where = "WHERE photo_collections.collection_id=$1";
+    res = await query(`${select} ${join} ${where}`, [collection_id]);
+  } else {
+    res = await query(select);
+  }
+  return res.rows;
+};
+
+
+module.exports = {
+  getPhotos,
+};
