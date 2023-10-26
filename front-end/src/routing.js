b58dac3f8091ec4c8b73df8a75671d7de1f01c7a
Chris Mastris <cmastris@hotmail.com>
2023-10-26 23:24:43 +0100

    Implement React Router error page

--- front-end/src/routing.js
+++ front-end/src/routing.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App/App";
+import FallbackErrorPage from "./components/FallbackErrorPage/FallbackErrorPage";
 
 
 // https://reactrouter.com/en/main/routers/create-browser-router
@@ -8,5 +9,6 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <FallbackErrorPage />,
   },
 ]);
