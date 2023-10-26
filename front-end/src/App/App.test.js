114860f2e5a85a93f98e412660d66897e1edc9bc
Chris Mastris <cmastris@hotmail.com>
2023-10-26 23:16:10 +0100

    Configure testing with RTL and a RouterProvider

--- front-end/src/App/App.test.js
+++ front-end/src/App/App.test.js
@@ -1,8 +1,14 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+
+import { createRouterProvider } from "../testSetup/testRouters";
 import App from "./App";
 
+// https://testing-library.com/docs/react-testing-library/intro
+// https://testing-library.com/docs/react-testing-library/example-intro
+// https://github.com/testing-library/jest-dom
+
 test("App renders", () => {
-  render(<App />);
-  const text = screen.getByText(/Hello world!/i);
-  expect(text).toBeInTheDocument();
+  render(createRouterProvider(<App />));
+  expect(screen.getByText("Hello world!")).toBeInTheDocument();
 });
