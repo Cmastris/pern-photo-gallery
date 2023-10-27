b62b74d431f2a07adf901a57c868ed3c22cee70a
Chris Mastris <cmastris@hotmail.com>
2023-10-27 15:59:35 +0100

    Update PhotoFeed loader to return collection data

--- front-end/src/features/photos/PhotoFeed.js
+++ front-end/src/features/photos/PhotoFeed.js
@@ -1,5 +1,23 @@
-async function fetchPhotos() {
-  const fetchURL = `${process.env.REACT_APP_API_BASE_URL}/photos`;
+async function fetchCollectionData(collectionSlug) {
+  const fetchURL = `${process.env.REACT_APP_API_BASE_URL}/collections/${collectionSlug}`;
+  const res = await fetch(fetchURL);
+  if (!res.ok) {
+    if (res.status === 404) {
+      // https://reactrouter.com/en/main/route/error-element#throwing-manually
+      throw new Response("Not Found", { status: 404 });
+    }
+    throw new Error("Unsuccessful collection fetch.");
+  }
+  const collectionData = await res.json();
+  return collectionData;
+}
+
+
+async function fetchPhotos(collectionId = null) {
+  let fetchURL = `${process.env.REACT_APP_API_BASE_URL}/photos`;
+  if (collectionId) {
+    fetchURL += `?collection_id=${collectionId}`;
+  }
   const res = await fetch(fetchURL);
   if (!res.ok) {
     throw new Error("Unsuccessful photos fetch.");
@@ -8,14 +26,29 @@ async function fetchPhotos() {
   return photosData;
 }
 
-export async function photoFeedLoader() {
+
+export async function photoFeedLoader({ params }) {
   // https://reactrouter.com/en/main/route/loader
+  let collectionData;
+  let photosData;
   try {
-    const photosData = await fetchPhotos();
+    if (params.slug) {
+      // Fetch collection data and filtered photo data
+      collectionData = await fetchCollectionData(params.slug);
+      photosData = await fetchPhotos(collectionData.id);
+    } else {
+      // Fetch all photos data
+      photosData = await fetchPhotos();
+    }
+    console.log(collectionData);
     console.log(photosData);
-    return { photosData };
+    return { collectionData, photosData };
 
   } catch (error) {
+    if (error.status === 404) {
+      throw error;  // Serve 404 error page
+    }
+    console.log('Error caught');
     return { error: "Sorry, photos could not be loaded." };
   }
 }
--- front-end/src/routing.js
+++ front-end/src/routing.js
@@ -20,6 +20,7 @@ export const routingConfig = [
       {
         path: "collections/:slug",
         element: <PhotoFeed />,
+        loader: photoFeedLoader,
       },
     ]
   },
