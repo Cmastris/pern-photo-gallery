01d6c62e6595392be2a5e229f72d45b9945a16e9
Chris Mastris <cmastris@hotmail.com>
2023-10-28 15:19:54 +0100

    Implement PhotoFeedItem

--- front-end/src/features/photos/PhotoFeedItem.js
+++ front-end/src/features/photos/PhotoFeedItem.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
+
 export default function PhotoFeedItem({ photoData }) {
+
+  const { title, slug, filename } = photoData;
+
   return (
     <article>
-      <p>{photoData.title}</p>
+      <Link to={`/photos/${slug}`}>
+        <img src={`/photo-images/preview/${filename}`} alt={title}></img>
+      </Link>
     </article>
   );
 }
