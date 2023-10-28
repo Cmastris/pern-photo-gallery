daf1ce4b1cfb948887fc8f785229d14535645e36
Chris Mastris <cmastris@hotmail.com>
2023-10-28 17:46:22 +0100

    Set high `fetchpriority` for first 2 feed images

--- front-end/src/features/photos/PhotoFeedItem.js
+++ front-end/src/features/photos/PhotoFeedItem.js
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 export default function PhotoFeedItem({ photoData, feedIndex }) {
 
   const { title, slug, filename } = photoData;
-  const imgSrc = `/photo-images/preview/${filename}`;
 
   return (
     <article>
       <Link to={`/photos/${slug}`}>
-        {/* Lazily load images except for the first 4 in a feed */}
-        <img src={imgSrc} alt={title} loading={feedIndex > 3 ? "lazy" : null}></img>
+        {/* Prioritise loading of first 2 images and lazily load images beyond the first 4 */}
+        <img
+          src={`/photo-images/preview/${filename}`}
+          alt={title}
+          fetchpriority={feedIndex < 2 ? "high" : null}
+          loading={feedIndex > 3 ? "lazy" : null}
+        ></img>
       </Link>
     </article>
   );
