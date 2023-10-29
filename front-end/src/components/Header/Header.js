028870ff4f5ece6b43545d18e191d9222f741d51
Chris Mastris <cmastris@hotmail.com>
2023-10-29 18:36:12 +0000

    Style Header & MainNav layout

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+
 import MainNav from "../MainNav/MainNav";
+import styles from "./Header.module.css";
 
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <Link to="/">Photo Gallery</Link>
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Link to="/" className={styles.logo}>Photo Gallery</Link>
       </div>
       <MainNav />
     </header>
--- front-end/src/components/MainNav/MainNav.js
+++ front-end/src/components/MainNav/MainNav.js
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
+import styles from "./MainNav.module.css";
 
 
 export default function MainNav() {
 
   function renderNavItem(path, anchor) {
     return (
-      <li>
-        <NavLink to={path}>{anchor}</NavLink>
+      <li className={styles.listItem}>
+        <NavLink
+        to={path}
+        className={styles.link}
+        >{anchor}</NavLink>
       </li>
     );
   }
 
   return (
-    <nav>
-      <ul>
+    <nav className={styles.mainNav}>
+      <ul className={styles.navList}>
         {renderNavItem("/", "Home")}
         {renderNavItem("/collections/landscapes", "Landscapes")}
         {renderNavItem("/collections/waterscapes", "Waterscapes")}
